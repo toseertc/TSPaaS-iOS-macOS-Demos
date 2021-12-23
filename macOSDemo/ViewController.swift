9c3ad786a32286776b0de1ee4f58b4cc1de64f5b
yaoxiaobing <xiaobing.yao@duobei.com>
2021-12-23 10:25:52 +0800

    feat: init

---
+++ macOSDemo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  macOSDemo
+//
+//  Created by yxibng on 2021/12/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
